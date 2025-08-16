8d4ddd567462086ef502b5dedd0cb38fb43a1191
choprapranay <pranay.chopra@mail.utoronto.ca>
2025-08-16 14:43:31 -0400

    finish map styling completly. integrated personalized style to Google Maps API and accounted for user restricitons

---
+++ app/map/page.tsx
@@ -0,0 +1,9 @@
+import NewMap from "@/components/NewMap";
+
+export default function MapPage() {
+    return (
+        <main className="container mx-auto p-6">
+            <NewMap />
+        </main>
+    );
+}
