4a8ed92d873944a35d7e8842c457f426a3d8dba7
Pranay Chopra <pchopra@pictoninvestments.com>
2026-08-13 14:56:09 -0400

    reset portfolio to redo the entire thing

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewMap from "@/components/NewMap";
-
-export default function Home() {
-    return (
-        <main className="container mx-auto p-6">
-            <NewMap />
-        </main>
-    );
-}
